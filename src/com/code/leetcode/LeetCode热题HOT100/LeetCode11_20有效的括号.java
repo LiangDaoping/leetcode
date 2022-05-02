6d650da7cb32e08f3d19a9364a3fbdd84ffa1092
gz <17806256336@163.com>
2022-05-02 16:18:35 +0800

    feat(LeetCode热题HOT100_第11个_20有效的括号)

--- src/com/code/leetcode/LeetCode热题HOT100/LeetCode11_20有效的括号.java
+++ src/com/code/leetcode/LeetCode热题HOT100/LeetCode11_20有效的括号.java
@@ -1,8 +1,5 @@
 package com.code.leetcode.LeetCode热题HOT100;
 
-
-import java.util.ArrayDeque;
-import java.util.Queue;
 import java.util.Stack;
 
 /**
@@ -26,34 +23,118 @@ public class LeetCode11_20有效的括号 {
 
         s = "{[]}";
         System.out.println(isValid(s)); // true
+
+        s = "(){}}{";
+        System.out.println(isValid(s)); // false
     }
 
     public static boolean isValid(String s) {
         if (s == null || s.length() == 0) {
             return true;
         }
-        Queue<Character> queue = new ArrayDeque<>();
-        Stack<Character> characters= new Stack<>();
-        characters.add('1');
-        queue.add(s.charAt(0));
-        int index = 1;
-        while (!queue.isEmpty() && index < s.length()) {
-            Character poll = queue.peek();
-            if (poll == '{' && s.charAt(index) == '}') {
-                queue.poll();
-                continue;
-            }
-            if (poll == '(' && s.charAt(index) == ')') {
-                queue.poll();
-                continue;
-            }
-            if (poll == '[' && s.charAt(index) == ']') {
-                queue.poll();
-                continue;
-            }
-            queue.add(s.charAt(index));
-            index++;
+        if (s.length() % 2 == 1) {
+            return false;
         }
-        return queue.isEmpty();
+        Stack<Character> stack = new Stack<>();
+        char[] charArray = s.toCharArray();
+        int index = 0;
+        while (index < charArray.length) {
+            char c = charArray[index++];
+            if (c == '(') {
+                stack.add(')');
+            } else if (c == '{') {
+                stack.add('}');
+            } else if (c == '[') {
+                stack.add(']');
+            } else if (stack.isEmpty() || stack.pop() != c) {
+                return false;
+            }
+        }
+        return stack.isEmpty();
+    }
+
+    public static boolean isValid3(String s) {
+        if (s == null || s.length() == 0) {
+            return true;
+        }
+        Stack<Character> stack = new Stack<>();
+        char[] charArray = s.toCharArray();
+        int index = 0;
+        while (index < charArray.length) {
+            char c = charArray[index++];
+            if (c == '(') {
+                stack.add(')');
+            } else if (c == '{') {
+                stack.add('}');
+            } else if (c == '[') {
+                stack.add(']');
+            } else if (stack.isEmpty() || stack.peek() != c) {
+                return false;
+            } else if (stack.peek() == c) {
+                stack.pop();
+            }
+        }
+        return stack.isEmpty();
+    }
+
+    public static boolean isValid2(String s) {
+        if (s == null || s.length() == 0) {
+            return true;
+        }
+        Stack<Character> stack = new Stack<>();
+        char[] charArray = s.toCharArray();
+        int index = 0;
+        while (index < charArray.length) {
+            if (stack.isEmpty() || charArray[index] == '(' || charArray[index] == '{' || charArray[index] == '[') {
+                stack.add(charArray[index++]);
+                continue;
+            }
+            Character peek = stack.peek();
+            if (peek == '(' && charArray[index] == ')') {
+                stack.pop();
+                index++;
+            } else if (peek == '[' && charArray[index] == ']') {
+                stack.pop();
+                index++;
+            } else if (peek == '{' && charArray[index] == '}') {
+                stack.pop();
+                index++;
+            } else {
+                return false;
+            }
+        }
+        return stack.isEmpty();
+    }
+
+    public static boolean isValid1(String s) {
+        if (s == null || s.length() == 0) {
+            return true;
+        }
+        Stack<Character> stack = new Stack<>();
+        char[] charArray = s.toCharArray();
+        stack.add(charArray[0]);
+        int index = 1;
+        while (index < charArray.length) {
+            if (stack.isEmpty() || !isMatch(stack.peek(), charArray[index])) {
+                stack.add(charArray[index++]);
+            } else {
+                stack.pop();
+                index++;
+            }
+        }
+        return stack.isEmpty();
+    }
+
+    public static boolean isMatch(char a, char b) {
+        if ((a == '(' && b == ')')) {
+            return true;
+        }
+        if ((a == '{' && b == '}')) {
+            return true;
+        }
+        if ((a == '[' && b == ']')) {
+            return true;
+        }
+        return false;
     }
 }
