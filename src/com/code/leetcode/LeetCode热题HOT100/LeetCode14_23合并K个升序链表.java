eb009cb1f2bedf67b8cd5555822fe8323bdd147e
gz <17806256336@163.com>
2022-06-15 20:41:15 +0800

    feat(LeetCode14_23合并K个升序链表)

---
+++ src/com/code/leetcode/LeetCode热题HOT100/LeetCode14_23合并K个升序链表.java
@@ -0,0 +1,30 @@
+package com.code.leetcode.LeetCode热题HOT100;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * @description: 23合并K个升序链表
+ * @author: gz
+ * @date: 2022/03/14
+ */
+public class LeetCode14_23合并K个升序链表 {
+    public static void main(String[] args) {
+//        [[1,4,5],[1,3,4],[2,6]]
+//        List<List<Integer>> lists = new ArrayList(){{1,4,5}};
+
+    }
+
+    public static ListNode mergeKLists(ListNode[] lists) {
+        return null;
+    }
+
+    static class ListNode {
+        int val;
+        ListNode next;
+        ListNode() {}
+        ListNode(int val) { this.val = val; }
+        ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+    }
+
+}
